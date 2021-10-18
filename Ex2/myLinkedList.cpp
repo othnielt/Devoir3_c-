1a4c9e5bbafcc1bc5639e428cab10c7b0760f135
Othniel Tiendrebeogo <othnieltiendrebeogo@Othniels-MacBook-Pro.local>
2021-10-18 01:37:20 -0400

    message

--- Ex2/myLinkedList.cpp
+++ Ex2/myLinkedList.cpp
@@ -6,7 +6,8 @@ int main()
 {
 	Evaluation* first = NULL;	//Pointer on the first element of the list
 						//the list is empty, so initialization to NULL
-	int number = 0;			//number of elements in the linked  list 
+	int number = 0;	  //number of elements in the linked  list 
+	 	
 	int choice;			//choice for the menu
 
 	do
@@ -23,7 +24,8 @@ int main()
 		{
 		case 1: display(first);
 			break;
-		case 2: first = add(first, number);
+		case 2: 
+		first = add(first, number);
 			break;
 		case 3: first = remove(first, number);
 			break;
@@ -41,10 +43,99 @@ int main()
 
 /*
 *add() Function*
+
+char student[capacity];
+	int grade;
+	Evaluation* next;
 **/
-Evaluation* add(Evaluation* p, int& number)
+Evaluation* add(Evaluation*  p, int& number)
 {
-	//VOTRE CODE VIENT ICI
+
+
+int compte = 1; 
+int studentGrade = 0;
+char studentName[20]; 
+Evaluation* loup;
+Evaluation* x ; 
+Evaluation* temps;
+Evaluation* suivant  = new Evaluation;
+loup = x = temps= p;
+
+
+
+// student name
+cout << "  student name : " ;
+
+ cin>>studentName;
+
+ suivant->student[20] = studentName[20];
+
+//note
+cout << "sudent grade : " ;
+cin >> studentGrade;
+
+suivant->grade = studentGrade;
+
+
+// verifie les elemntes 
+if ( p  == NULL && number!= 1 || p != NULL && number == 1) {
+cout << "Invalide number of existing elements." ;
+return p;
+}
+else if (p != NULL) {
+while (loup != NULL) {
+
+temps = loup;
+loup = loup->next;
+compte++;
+}
+if (compte == number) {
+
+x = temps;
+}
+else if (compte-1 != number) {
+cout << "erreur ." ;
+return p;
+}
+}
+
+
+if (number == 1) {
+
+suivant->next = NULL;
+p  = suivant ;
+
+}
+
+else if (x ->next == NULL) {
+	
+
+x->next = suivant;
+suivant->next = NULL;
+}
+else if (x->next != NULL) {
+Evaluation* temp = x->next;
+x->next = suivant;
+suivant->next = temp;
+cout << x->student<<endl;
+}
+return p;
+}
+/*
+  
+ 
+
+	   
+    
+	 
+
+	
+     
+	 
+
+
+
+
 }
 
 
@@ -55,7 +146,87 @@ Evaluation* add(Evaluation* p, int& number)
 Evaluation* remove(Evaluation* p, int& number)
 {
 	//VOTRE CODE VIENT ICI
+ int compte = 1; 
+
+Evaluation* loup;
+Evaluation* x;
+Evaluation* temps;
+Evaluation* supprime;
+
+loup = x = temps = supprime= p ;
+
+if ( p == NULL || number ==0 ){
+
+cout << " position invalide ";
+return p ;
 }
+
+else {
+
+	while (loup != NULL ){
+
+		if (compte ==number){
+
+
+			supprime = loup;
+
+			x = temps;
+
+			break ; 
+		}
+
+		temps = loup;
+		loup = loup->next;
+		compte++;
+
+
+	}
+      int var = compte -1 ; 
+	if (var!= number ){
+
+
+		cout << "error position"<< endl; 
+
+		return p; 
+	}
+
+}
+
+		if (number == 1){
+
+
+			temps = p->next;
+			delete p; 
+			return temps ;
+		}
+
+		if (supprime->next ==NULL ){
+
+            x->next = NULL;
+			delete supprime; 
+
+		}
+
+		else {
+
+			x->next = supprime->next;
+		}
+
+
+
+
+
+ return p ; 
+
+	}
+
+
+
+
+
+
+
+
 
 
 
@@ -65,6 +236,20 @@ Evaluation* remove(Evaluation* p, int& number)
 void display(Evaluation* p)
 {
 	//VOTRE CODE VIENT ICI
+
+
+Evaluation* value  = p ; 
+	while (value != NULL ){
+
+
+		cout<< "Student: "<<endl;
+		cout<< value->student<<endl;
+		cout<< "the grade : "<<endl;
+		cout<< value->grade<<endl;
+
+		value  = value->next;
+
+	}
 }
 
 
@@ -74,10 +259,45 @@ void display(Evaluation* p)
 ***/
 int average(Evaluation* p, int const& nbre)
 {
-	//VOTRE CODE VIENT ICI
+	Evaluation*  loup = p ; // permet de parcourir la list
+
+	double total= 0  ; 
+    int compte = 0 ; 
+
+	if ( p == NULL || nbre ==0 ){
+
+		cout<< "0"<<endl;
+
+		return 0 ;
+	}
+
+
+	else {
+
+		 while ( loup  != NULL && compte !=nbre)
+		 {
+
+			 total = total + loup->grade; 
+			 loup = loup->next;
+			 compte ++;
+			 /* code */
+		 }
+
+		 if (nbre != compte){
+
+			 cout<<"errro "<<endl;
+		 }
+
+		 else{
+
+			 cout <<"The average of the class is: "<< total/compte;
+			 return 1; 
+		 }
+		 
+	}
+
+	
 }
-
-
 
 
 
--- Ex2/myLinkedList.h
+++ Ex2/myLinkedList.h
@@ -1,9 +1,10 @@
 /*Ex2 - Devoir 3 CSI2772A*/
 
 #include <iostream>
-#include <process.h>
+//#include <process.h>
 using namespace std;
 const int capacity = 20;	// size string
+int incrementation  =  0 ; // track the max of the LinkedList
 
 struct 	Evaluation
 {
@@ -12,8 +13,11 @@ struct 	Evaluation
 	Evaluation* next;
 };
 
-Evaluation* add(Evaluation*, int&);
+Evaluation* add(Evaluation*, int&);   // &T
 Evaluation* remove(Evaluation*, int&);
 void display(Evaluation*);
 int average(Evaluation*, int const&);
+Evaluation* head = NULL; //	the head of linkedList
+
+
 
